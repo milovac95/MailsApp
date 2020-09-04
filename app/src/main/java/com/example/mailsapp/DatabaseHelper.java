c4b342bdd70f123fdcac71ec1a50dc8a2572c683
s7mmilovac <marko.milovac@s7designcreative.com>
2020-09-04 14:44:40 +0200

    db insert function

--- app/src/main/java/com/example/mailsapp/DatabaseHelper.java
+++ app/src/main/java/com/example/mailsapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.mailsapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,7 +12,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
@@ -27,5 +27,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS email_table");
         db.execSQL("DROP TABLE IF EXISTS folder_table");
         onCreate(db);
+    }
+
+    public boolean insertData(String emailSubject, String emailTo, String emailContent){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("emailFrom", "milovac.business@gmail.com");
+        contentValues.put("emailFrom", emailSubject);
+        contentValues.put("emailTo", emailTo);
+        contentValues.put("content", emailContent);
+        long result = db.insert("email_table",null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+
     }
 }
