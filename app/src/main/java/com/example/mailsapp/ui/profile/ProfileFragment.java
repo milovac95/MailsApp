8f1c8d4cbdf09e611f16a3274d988e4008ae8f53
s7mmilovac <marko.milovac@s7designcreative.com>
2020-09-10 11:18:51 +0200

    change profiles logic

--- app/src/main/java/com/example/mailsapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mailsapp/ui/profile/ProfileFragment.java
@@ -2,6 +2,8 @@ package com.example.mailsapp.ui.profile;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +13,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.example.mailsapp.DatabaseHelper;
+import com.example.mailsapp.MailSingleActivity;
+import com.example.mailsapp.MainActivity;
+import com.example.mailsapp.Preferences;
 import com.example.mailsapp.R;
+import com.example.mailsapp.adapters.SentMailCursorAdapter;
+import com.example.mailsapp.adapters.UserCursorAdapter;
 
-public class ProfileFragment extends Fragment {
-
+public class ProfileFragment extends Fragment implements AdapterView.OnItemClickListener{
+    Cursor users;
+    DatabaseHelper myDb;
     private ProfileViewModel mViewModel;
 
     public static ProfileFragment newInstance() {
@@ -26,6 +37,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        myDb = new DatabaseHelper(getActivity());
         return inflater.inflate(R.layout.profile_fragment, container, false);
     }
 
@@ -33,7 +45,20 @@ public class ProfileFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
-        // TODO: Use the ViewModel
+        ListView profileList = (ListView) getActivity().findViewById(R.id.profile_list);
+        users= myDb.getAllUsers();
+        UserCursorAdapter userCursorAdapter = new UserCursorAdapter(getActivity(), R.layout.profile_list_item, users, 0);
+        profileList.setAdapter(userCursorAdapter);
+        profileList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String userId = users.getString(0);
+
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        Preferences.setPreferencesUserId(getActivity(), userId);
+        startActivity(intent);
     }
 
 }
